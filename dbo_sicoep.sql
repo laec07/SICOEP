4e0f638646167adbf0c6be1e9c3b7fdb59e5cba5
laec07 <66032859+laec07@users.noreply.github.com>
2020-09-09 20:40:54 -0600

    Add files via upload

---
+++ dbo_sicoep.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : local
+ Source Server Type    : MySQL
+ Source Server Version : 100413
+ Source Host           : localhost:3306
+ Source Schema         : dbo_sicoep
+
+ Target Server Type    : MySQL
+ Target Server Version : 100413
+ File Encoding         : 65001
+
+ Date: 09/09/2020 20:38:38
+*/
+
